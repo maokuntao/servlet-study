d8c6665ec0c7a1a12b96173d055856efcfa1b3ec
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-07-06 23:08:45 +0800

    add

--- src/com/taomk/servlet/DateServlet.java
+++ src/com/taomk/servlet/DateServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 public class DateServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
-
+	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Date now = new Date();
